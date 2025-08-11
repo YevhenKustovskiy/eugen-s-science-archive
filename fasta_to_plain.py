b10a60c00556c11a174f0ba38d7a992303dda8cb
YevhenKustovskiy <136888021+YevhenKustovskiy@users.noreply.github.com>
2025-08-11 19:10:14 +0300

    Update fasta_to_plain.py

--- fasta_to_plain.py
+++ fasta_to_plain.py
@@ -1,3 +1,29 @@
+"""mdhbonds: in batch converts fasta files with aminoacid sequences to plain text (lines with no symbols other than aminoacids) 
+   Copyright (C) 2024  Yevhen Kustovskiy
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. 
+   
+   A copy of GNU license as well as NumPy and Pandas license is available at:
+   
+   https://github.com/YevhenKustovskiy/eugen-s-science-archive/edit/main/LICENSE   
+   
+   Contact email: ykustovskiy@gmail.com
+   
+   Requirements: Python 3.11
+                                                                          """
+
 import os
 import argparse
 
@@ -39,4 +65,5 @@ if __name__ == "__main__":
     elif args.d:
         process_folder(CWD)
                 
+
         
